d3ddd653b4a3fd48552b09589c03a53bf79b1d18
Blaster911 <romain588@live.fr>
2022-05-25 17:36:55 +0200

    refactored

--- account-management.py
+++ account-management.py
@@ -3,7 +3,7 @@ import crayons
 
 
 def calculVir(nbr, percentage):  # Function pour calculer le découpage du virement
-    nbr = int(nbr)
+    nbr = float(nbr)
     percentage = float(percentage)
     calc = round(nbr - (nbr * percentage/100) - nbr, 1)
     return float(calc)
@@ -13,7 +13,6 @@ def printCrGreen(string):
     return print('{}'.format(crayons.green(string)))
 
 
-print('{}'.format(crayons.green()))
 # Lecture du fichier.ini
 config = configparser.ConfigParser()
 config.read('my-accounts.ini')
@@ -22,18 +21,16 @@ config.read('my-accounts.ini')
 print(" ")
 vir = input("Rentrer le montant du virement : ")
 rest = float(vir)
-print("=====€=====")
-print(" ")
+print("=====€=====\n")
 # On boucle sur les différentes valeurs du fichier et on calcul le tout
 for key in config['COMPTE']:
     readPercentage = float(config['COMPTE'][key])
     withdrew = calculVir(vir, readPercentage)
     rest = rest + withdrew
     printCrGreen("Montant à retiré et transférer sur le compte " +
-                 str(key) + " : ") + crayons.red(str(withdrew) + "€")
+                 str(key) + " : " + crayons.red(str(withdrew) + "€\n"))
     # print("Montant à retiré pour transférer sur le compte " +
     #       str(key) + " : " + str(withdrew) + "€")
-print("Reste : " + crayons.yellow(str(round(rest, 1)) + "€"))
-print(" ")
-print("=====€=====")
+print("Reste : " + crayons.yellow(str(round(rest, 1)) + "€\n"))
+print("=====€=====\n")
 input("Appuyer sur Entrer pour terminer...")
