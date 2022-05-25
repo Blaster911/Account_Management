771f2f6d13be2358d54a9f355b340f2ff2287103
Blaster911 <romain588@live.fr>
2022-05-26 01:06:53 +0200

    add create file

--- account-management.py
+++ account-management.py
@@ -1,6 +1,9 @@
 import configparser
 import crayons
 
+# variable
+file = "my-accounts.ini"
+
 
 def calculVir(nbr, percentage):  # Function pour calculer le découpage du virement
     nbr = float(nbr)
@@ -14,13 +17,22 @@ def log(string):
 
 
 # Lecture du fichier.ini
-config = configparser.ConfigParser()
-config.read('my-accounts.ini')
+with open(file, 'a') as configfile:
+    config = configparser.ConfigParser()
+    config.read(file)
+    try:
+        config.add_section('COMPTE')
+        config.set('COMPTE', 'CA', '20')
+        config.write(configfile)
+        log("Création du fichier " + file + "...")
+    except:
+        log("Lecture du fichier " + file + "...")
 
 # insertion du montants / tableau des restes
 vir = input("Rentrer le montant du virement : ")
 rest = float(vir)
 log("=====€=====\n")
+
 # On boucle sur les différentes valeurs du fichier et on calcul le tout
 for key in config['COMPTE']:
     readPercentage = float(config['COMPTE'][key])
@@ -28,8 +40,7 @@ for key in config['COMPTE']:
     rest = rest + withdrew
     log(crayons.green("Montant à retiré et transférer sur le compte ") +
         crayons.yellow(str(key)) + " : " + crayons.red(str(withdrew) + "€\n"))
-    # print("Montant à retiré pour transférer sur le compte " +
-    #       str(key) + " : " + str(withdrew) + "€")
+
 log("Reste : " + crayons.yellow(str(round(rest, 1)) + "€\n"))
 log("=====€=====\n")
 input("Appuyer sur Entrer pour terminer...")
